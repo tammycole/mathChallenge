07365790b908b684caa54605fe4521aa7de45af5
Tammy <tammymarie.cole@gmail.com>
2017-08-18 11:36:57 -0400

    clean up comments

--- MathChallenge/Program.cs
+++ MathChallenge/Program.cs
@@ -10,41 +10,42 @@ namespace MathChallenge
 
             //request first number from user
             Console.WriteLine("Enter a whole number:");
-            //int number1 = Convert.ToInt32(Console.ReadLine());
-
             string value = Console.ReadLine();
             int number1;
             string notInt = "This is not an integer.";
 
+            //validate that input is an integer
             bool successfullyParsed = int.TryParse(value, out number1);
             if (successfullyParsed)
             {
                 int successful = number1;
             }
             else
+            {
                 Console.WriteLine(notInt);
-             
+                Console.ReadKey();
+            }
 
             //request second number from user
             Console.WriteLine("Enter another number with the same number of digits:");
-            // var number2 = Convert.ToInt32(Console.ReadLine());
             string value2 = Console.ReadLine();
             int number2;
 
+            //validate that input is an integer
             bool successfullyParsed2 = int.TryParse(value2, out number2);
             if (successfullyParsed2)
             {
                 int successful = number2;
             }
             else
+            {
                 Console.WriteLine(notInt);
+                Console.ReadKey();
+            }
 
             //get lengths of each number
             int length1 = number1.ToString().Length;
-            // Console.WriteLine(length1);
-
             int length2 = number2.ToString().Length;
-            //Console.WriteLine(length2);
 
             string userInput;
             string Answer;
@@ -69,27 +70,19 @@ namespace MathChallenge
                 int lastDigitX;
 
                 lastDigitI = (i % 10);
-                //Console.WriteLine(lastDigitI);
                 lastDigitX = (x % 10);
-                //Console.WriteLine(lastDigitX);
 
                 //get comparison sum for first pair of digits
                 int sumStatic = lastDigitI + lastDigitX;
-                // Console.WriteLine(sumStatic);
 
                 //sum of remaining pairs in do while loop
                 do
                 {
                     lastDigitI = (i % 10);
-                    //Console.WriteLine(lastDigitI);
                     lastDigitX = (x % 10);
-                    //Console.WriteLine(lastDigitX);
                     i = i / 10;
-                    //Console.WriteLine(i);
                     x = x / 10;
-                    // Console.WriteLine(x);
                     l--;
-                    //Console.WriteLine(l);
                     int sumCompare = lastDigitI + lastDigitX;
 
                     if (sumCompare != sumStatic)
